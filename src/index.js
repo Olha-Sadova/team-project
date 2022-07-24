2ee95b7e0f5a5e63a7dca1e839e0b468210fe12f
Nazar Kobernyk <nazarkoko@icloud.com>
2022-07-24 17:46:36 +0100

    fixed sсss rules

---
+++ src/index.js
@@ -0,0 +1,16 @@
+
+
+//---Swiper-slider
+
+
+var swiper = new Swiper('.mySwiper', {
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  effect: 'cube',
+  cubeEffect: {
+    slideShadows: false,
+  },
+});
