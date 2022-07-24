bb64179dc1d7e339ed2ee05d44edc8e8abd0420f
Olha-Sadova <105297755+Olha-Sadova@users.noreply.github.com>
2022-07-24 21:21:46 +0000

    Deploying to gh-pages from @ Olha-Sadova/team-project@85ebb70e8bcb1604b1581d71819090cbd87ed74c 🚀

---
+++ index.79feab4a.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0},effect:"cube",cubeEffect:{slideShadows:!1}});
+//# sourceMappingURL=index.79feab4a.js.map
---
+++ index.987be88b.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{spaceBetween:30,pagination:{el:".swiper-pagination",clickable:!0},effect:"cube",cubeEffect:{slideShadows:!1}});
+//# sourceMappingURL=index.987be88b.js.map
