d5afbd0236d22f5fcdf7d3b686e072aedacfc5d1
Юлия Бакай <105805631+yuliabakay@users.noreply.github.com>
2022-07-27 22:45:20 +0300

    changed menu

--- src/menu.js
+++ src/menu.js
@@ -4,9 +4,11 @@
     closeMenuBtn: document.querySelector('.mobile-close-button'),
     menu: document.querySelector('.mobile-menu'),
     body: document.querySelector('body'),
+    mobMenuList: document.querySelector('.header-mob-list'),
   };
   refs.openMenuBtn.addEventListener('click', toggleMenu);
   refs.closeMenuBtn.addEventListener('click', toggleMenu);
+  refs.mobMenuList.addEventListener('click', toggleMenu);
   function toggleMenu() {
     refs.menu.classList.toggle('is-hidden');
     refs.body.classList.toggle('no-scroll');
