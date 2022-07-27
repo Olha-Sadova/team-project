e13757f29ce7e46fc37d5ad80ec1fe74d43d7fdb
Olha-Sadova <105297755+Olha-Sadova@users.noreply.github.com>
2022-07-27 20:20:05 +0000

    Deploying to gh-pages from @ Olha-Sadova/team-project@c4d3c1a440c5f39c5c677cbf84144da05efbba88 🚀

--- index.aea08b76.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector(".header-mobile-menu"),closeMenuBtn:document.querySelector(".mobile-close-button"),menu:document.querySelector(".mobile-menu"),body:document.querySelector("body")};function n(){e.menu.classList.toggle("is-hidden"),e.body.classList.toggle("no-scroll")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.aea08b76.js.map
