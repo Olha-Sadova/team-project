fcec4536d8a28b5e086098b0799deee396c8e0c9
Olha-Sadova <105297755+Olha-Sadova@users.noreply.github.com>
2022-07-28 08:12:27 +0000

    Deploying to gh-pages from @ Olha-Sadova/team-project@e5f109ecd41a83fd52d1cc3ea9035f841c86934c 🚀

--- index.25508877.js
+++
@@ -1,2 +0,0 @@
-$(document).ready((function(){$("#hero-list").on("click","a",(function(t){t.preventDefault();var o=$(this).attr("href"),e=$(o).offset().top;$("body,html").animate({scrollTop:e},1500)}))}));
-//# sourceMappingURL=index.25508877.js.map
---
+++ index.5e95e2ec.js
@@ -0,0 +1,2 @@
+$(document).ready((function(){$("#hero-list").on("click","a",(function(t){t.preventDefault();var o=$(this).attr("href"),e=$(o).offset().top;$("body,html").animate({scrollTop:e},500)}))}));
+//# sourceMappingURL=index.5e95e2ec.js.map
